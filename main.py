549aafd6a3fbc784e35110649376709df901807f
DanTheMan3721 <dli20060805@gmail.com>
2026-01-27 22:07:44 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,18 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+api_key = os.environ.get("AIzaSyCE8y87en0Kgclk1MGn9CAl_z-XD69485o")
+
+from google import genai
+
+client = genai.Client(api_key=api_key)
+
+response = client.models.generate_content(
+    model='gemini-2.5-flash', contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
+)
+print(response.text)
+
 def main():
     print("Hello from Build an AI agent in Python!")
 
